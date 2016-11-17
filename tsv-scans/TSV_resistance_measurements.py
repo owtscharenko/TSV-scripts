edb17543ba395f66c2e78a52470d402b1eb0898d
owtscharenko <niko@silab-32>
2016-11-17 14:43:05 +0100

    added parameters for fit in "plot.py" also now prints mean Resistance

--- tsv-scans/TSV_resistance_measurements.py
+++ tsv-scans/TSV_resistance_measurements.py
@@ -4,6 +4,7 @@ from plots import TSV_res_meas_analysis
 import logging
 import os
 import time
+import numpy as np
 
 
 
@@ -15,7 +16,7 @@ if __name__ == '__main__':
 #     current = -0.02 # In Ampere
     voltage = 0.21  # In VOLT
     climit = 0.3 # In Ampere
-    marker = True
+    marker = False
     stepsize = 0.0005
     
     '''creating file name'''
@@ -32,7 +33,7 @@ if __name__ == '__main__':
 
     
     f = 'via' + str(via) + '-' + str(int(climit*1000)) + 'mamp-4wire.csv'
-    workdir = '/media/niko/data/Niko/TSV-D3/resmeas2'
+    workdir = '/media/niko/data/Niko/TSV-D5/resmeas'
     os.chdir(workdir)
 
     '''
@@ -43,17 +44,19 @@ if __name__ == '__main__':
 #         iv.scan_tsv_res_source_CURR(f, current, 1, 5000 , 0.0001, sensing, 'Sourcemeter1')
 #     elif current < 0 :
     file =  iv.scan_tsv_res_source_VOLT(f, voltage, climit, 1, 5000 , stepsize, sensing, 'Sourcemeter1')                                                                  
-    
+#     
     time.sleep(0.05)
     '''
     Plotting
     '''   
+    p = (1,5,7);
     func = TSV_res_meas_analysis()
 #     print workdir, f
     x,y,z = func.load_file(os.path.join(workdir, file))
     
-    func.plot_single_via(x, y, z, marker)     
+    func.plot_single_via(x, y, z, p, marker)     
 #     print func.mean_res_1_via(z)
     func.histo_1_via(z,60,'b')
+    print np.mean(z[40:])
     logging.info('plotting finished')
           
