52df96709fb09558979c79fab284a2933ffaa792
niko <niko@silab32>
2016-12-06 10:26:22 +0100

    new: function for measurement(debouncing)

--- tsv-scans/measurement_loop.py
+++ tsv-scans/measurement_loop.py
@@ -20,6 +20,51 @@ class IV(object):
         #self.misc = Misc(dut=self.dut)  
 
 
+
+    def measure_current(self, device, timeout,minimum_delay):
+        
+        was_above = False
+        was_below = True
+        
+        current = float(self.dut[device].get_current())
+        for i in range(timeout):
+            time.sleep(minimum_delay)
+            measurement = float(self.dut[device].get_current())
+            if abs(measurement) > abs(current):
+                was_above = True
+            if abs(measurement) < abs(current):
+                was_below = True
+            if was_above and was_below:
+                break
+            if measurement == current:
+                break
+            current = measurement
+            
+        return measurement
+
+    def measure_voltage(self, device, timeout, minimum_delay):
+        
+        was_above = False
+        was_below = True
+        
+        voltage = float(self.dut[device].get_voltage())
+        for i in range(timeout):
+            time.sleep(minimum_delay)
+            measurement = float(self.dut[device].voltage())
+            if abs(measurement) > abs(voltage):
+                was_above = True
+            if abs(measurement) < abs(voltage):
+                was_below = True
+            if was_above and was_below:
+                break
+            if measurement == voltage:
+                break
+            voltage = measurement
+            
+        return measurement
+
+
+
     def scan_tsv_res_VOLT(self, file_name, max_Vin, current_limit, steps , stepsize, *device):
           
             logging.info("Starting ...")              
@@ -31,8 +76,7 @@ class IV(object):
                 file_name = file_name.split('_')[0]
                 file_name = file_name + "_" + str(fncounter) + ".csv"
                 fncounter = fncounter + 1
-                
-                
+                              
             pbar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', progressbar.AdaptiveETA()], maxval=max_Vin, poll=10, term_width=80).start()               #fancy progress bar
             with open(file_name, 'wb') as outfile:
                 f = csv.writer(outfile ,quoting=csv.QUOTE_NONNUMERIC)
@@ -40,18 +84,18 @@ class IV(object):
                 f.writerow(['Input voltage [V]', 'Input current [A]', 'Resistance [ohm] '])                      #What is written in the output file
             
                 input = 0
-                counter = 0                                                                                          #Check input current
+                counter = 0                                                                                 
                 for x in range(0, int(steps)):                                                                                                                #loop over steps                                                                
                     self.dut['Sourcemeter'].set_voltage(input)
                     self.dut['Sourcemeter'].on()                                                                                                  #Set input current
-                    input_voltage = float(self.dut['Sourcemeter'].get_voltage())
+                    input_voltage = self.measure_voltage('Sourcemeter', 100, 0.05)
                     #self.dut['Sourcemeter'].on()                                                                                       #Value of interest is in the [0] position of the readout list
-                    input_current = float(self.dut['Sourcemeter'].get_current())
+                    input_current = self.measure_current('Sourcemeter', 100 , 0.05)    
                     resistance = input_voltage/input_current
                     logging.info("Set input voltage to %r" % input)
                     logging.info("Input current is %r A" % input_current)                                                                                           #Logging the readout
                     logging.info("Input voltage is %r V" % input_voltage)
-                    print 'resistance %r' % resistance                                                         #Writing readout in output file
+#                     print 'resistance %r' % resistance                                                         #Writing readout in output file
                     if x > 0 and input_current > 0 :
                         self.data.append([input_voltage, input_current, resistance])                                                          #Writing readout in output file
                         f.writerow(self.data[-1])
@@ -59,7 +103,7 @@ class IV(object):
                         print'warning: outside range! current = %f' % input_current
                     elif x == 0 :
                         print 'discarding first value'
-                    if resistance > 1e7:
+                    if resistance > 1e5 :
                         counter +=1
                     pbar.update(input)
                     input += stepsize                                                                                                                             #Increase input current for next iteration
@@ -73,8 +117,6 @@ class IV(object):
                         break     
                     elif counter > 0:
                         print 'counter = %i' % counter
-                     
-                
                 
                 pbar.finish()
                 self.dut['Sourcemeter'].off()
